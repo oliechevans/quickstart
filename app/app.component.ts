a0f07311016b41a14a573954a7ffadb276990b53
Evans O Oliech <oliechevans@gmail.com>
2016-11-12 00:00:56 -0500

    start

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    selector: 'pm-app',
+    template: `
+    <div>
+    <h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+    </div>
+    `
+    
 })
-export class AppComponent { }
+export class AppComponent { 
+    pageTitle:string="Acme Product Management";
+}
