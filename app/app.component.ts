304a1aaf84e050bd2d18436fc99de0898fae7c3c
Evans O Oliech <oliechevans@gmail.com>
2016-11-14 17:16:12 -0500

    update

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 
-@Component({
+//import { ProductListComponent } from './products/product-list.component';
+
+@Component({          
     selector: 'pm-app',
     template: `
     <div>
-    <h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
+    <h1>{{pageTitle}}</h1>    
+        <pm-products></pm-products>
     </div>
-    `
-    
+    `      
+    //entryComponents : [ ProductListComponent ]
+   //directive:[]     
 })
+
+
 export class AppComponent { 
     pageTitle:string="Acme Product Management";
 }
