fe8e3fd933ac39660c20a375be7dc2be8d1a1994
Evans Oliech <eoliech@usli.local>
2016-11-22 15:31:43 -0500

    ttt

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-
+import{IProduct} from './Product'
 
 
 @Component({
@@ -13,7 +13,7 @@ export class ProductListComponent{
     imageMargin:number=2;
     showImage:boolean=false;
     listFilter:string='cart';
-    products:any[] = [
+    products:IProduct[] = [
     {
         "productId": 1,
         "productName": "Leaf Rake",
